79e806b36abbe85e3051aa317d02b312c8624846
Nghia <nltnghia21@gmail.com>
2024-05-11 23:32:35 +0700

    update TicketFragment and TicketListViewFragment

---
+++ app/src/main/java/com/example/repcgv/fragments/TicketFragment.kt
@@ -0,0 +1,75 @@
+package com.example.repcgv.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.repcgv.MainActivity
+import com.example.repcgv.R
+import com.example.repcgv.adapters.ViewPagerTicketApdater
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class TicketFragment : Fragment() {
+    private lateinit var backBtn: ImageButton
+    private lateinit var menuBtn: ImageButton
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+
+    private lateinit var viewPagerAdapter: ViewPagerTicketApdater
+
+    private lateinit var findTicketBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_ticket, container, false)
+
+        init(view)
+
+        return view
+    }
+
+    private fun init(view: View){
+        backBtn = view.findViewById(R.id.backBtn)
+        menuBtn = view.findViewById(R.id.menuBtn)
+
+        backBtn.setOnClickListener {
+            (this.activity as? MainActivity)?.goBack()
+        }
+
+        menuBtn.setOnClickListener {
+            (this.activity as? MainActivity)?.openDrawer()
+        }
+
+        tabLayout = view.findViewById(R.id.tabLayout)
+        viewPager = view.findViewById(R.id.viewPager)
+
+        viewPagerAdapter = ViewPagerTicketApdater(this)
+        viewPager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when(position){
+                0 -> tab.text = "Upcoming movies"
+                1 -> tab.text = "Watched movies"
+                else -> tab.text = "How do you get to this?"
+            }
+        }.attach()
+
+        findTicketBtn = view.findViewById(R.id.findTicketBtn)
+        findTicketBtn.setOnClickListener {
+            
+        }
+    }
+}
---
+++ app/src/main/java/com/example/repcgv/models/Ticket.kt
@@ -0,0 +1,21 @@
+package com.example.repcgv.models
+import com.google.gson.annotations.SerializedName
+
+data class Ticket(
+    @SerializedName("order_id") val id: Int,
+    @SerializedName("movie_title") val movieName: String,
+    @SerializedName("movie_poster") val moviePoster: String,
+    @SerializedName("movie_classification") val movieClassification: String,
+    @SerializedName("room_id") val roomId: Int,
+    @SerializedName("cinema_name") val cinemaName: String,
+    @SerializedName("cinema_address") val cinemaAddress: String,
+    @SerializedName("schedule_date") val scheduleDate: Long,
+    @SerializedName("schedule_start") val scheduleStart: String,
+    @SerializedName("ticket") val ticket: List<String>,
+    @SerializedName("discount") val discount: Int,
+    @SerializedName("total") val total: Int,
+    @SerializedName("total_ticket") val totalTicket: Int,
+    @SerializedName("total_food") val totalFood: Int,
+    @SerializedName("payment_method") val paymentMethod: String
+) {
+}
