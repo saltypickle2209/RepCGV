d0c3328e6b8eaef625600f1c57b749f23c54f8b6
HenryDo203 <dophamthanhuy2842003@gmail.com>
2024-05-11 12:59:36 +0700

    update view and layout for Food Order

--- app/src/main/java/com/example/repcgv/adapters/RecyclerViewFoodAdapter.kt
+++ app/src/main/java/com/example/repcgv/adapters/RecyclerViewFoodAdapter.kt
@@ -1,258 +1,84 @@
-package com.example.repcgv.fragments
+package com.example.repcgv.adapters
 
-import android.content.Context
-import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.example.repcgv.MainActivity
+import com.bumptech.glide.Glide
 import com.example.repcgv.R
-import com.example.repcgv.adapters.RecyclerViewFoodAdapter
-import com.example.repcgv.api.AccountApi
-import com.example.repcgv.api.FoodApi
-import com.example.repcgv.api.OrderApi
-import com.example.repcgv.api.RetrofitClient
-import com.example.repcgv.api.ScheduleApi
 import com.example.repcgv.models.Food
-import com.example.repcgv.models.Movie
-import com.example.repcgv.models.Order
-import com.example.repcgv.models.Schedule
-import com.example.repcgv.models.User
-import com.google.gson.Gson
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.text.NumberFormat
 import java.util.Currency
 import java.util.Locale
-import java.util.UUID
-import kotlin.random.Random
 
-class FoodOrderFragment : Fragment() {
-    private lateinit var root: View
+class RecyclerViewFoodAdapter(private val fragment: Fragment, private val foods: List<Food>) :
+    RecyclerView.Adapter<RecyclerViewFoodAdapter.ViewHolder>() {
+    lateinit var onFoodAdd: ((Int) -> Unit)
+    lateinit var onFoodRemove: ((Int) -> Unit)
 
-    private lateinit var recyclerView: RecyclerView
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imagePoster = view.findViewById<ImageView>(R.id.imagePoster);
+        val textFoodName = view.findViewById<TextView>(R.id.textFoodName);
+        val textFoodDescription = view.findViewById<TextView>(R.id.textFoodDescription);
+        val buttonAdd = view.findViewById<Button>(R.id.btnAdd);
+        val buttonMinus = view.findViewById<Button>(R.id.btnMinus);
+        val textAmount = view.findViewById<TextView>(R.id.textAmount);
 
-    lateinit var adapter: RecyclerViewFoodAdapter
-
-    private lateinit var foodList: ArrayList<Food>
-
-    private val foodBought = hashMapOf<Int, Int>()
-
-    private var currentPrice: Double = 0.0;
-    private var selectedSeatID: String = "";
-    private var userId: String = "";
-    private lateinit var schedule: Schedule;
-    private var movieName = "";
-
-    private lateinit var textMovieName: TextView;
-    private lateinit var textScreenType: TextView;
-    private lateinit var textPrice: TextView;
-    private lateinit var textSeatSelected: TextView;
-
-    private lateinit var buttonPay: Button;
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        root = inflater.inflate(R.layout.fragment_food_order, container, false)
-
-        arguments?.takeIf { it.containsKey("schedule") }?.apply {
-            schedule = getSerializable("schedule") as Schedule
-        }
-        arguments?.takeIf { it.containsKey("selectedSeatID") }?.apply {
-            selectedSeatID = getString("selectedSeatID")!!
-        }
-        arguments?.takeIf { it.containsKey("currentPrice") }?.apply {
-            currentPrice = getDouble("currentPrice")
-        }
-        arguments?.takeIf { it.containsKey("movieName") }?.apply {
-            movieName = getString("movieName")!!
-        }
-
-        fetchData()
-        return root;
-    }
-
-    private fun init()
-    {
-        recyclerView = root.findViewById(R.id.recyclerView)
-
-        adapter = RecyclerViewFoodAdapter(this, foodList)
-        recyclerView.adapter = adapter;
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        adapter.onFoodAdd = { foodId ->
-            if (!foodBought.containsKey(foodId)) {
-                foodBought[foodId] = 1
-            } else {
-                foodBought[foodId] = foodBought[foodId]!! + 1
-            }
-            updateOrderDetails()
-        }
-
-        adapter.onFoodRemove = { foodId ->
-            if (foodBought.containsKey(foodId)) {
-                foodBought[foodId] = foodBought[foodId]!! - 1
-            }
-            updateOrderDetails()
-        }
-
-        textMovieName = root.findViewById(R.id.textMovieName)
-        textScreenType = root.findViewById(R.id.textScreenType)
-        textSeatSelected = root.findViewById(R.id.textSeatSelected)
-        textPrice = root.findViewById(R.id.textPrice)
-
-        buttonPay = root.findViewById(R.id.buttonPay)
-
-        buttonPay.setOnClickListener{
-            tryPostOrder()
-        }
-    }
-
-    private fun SubmitData()
-    {
-        val order = Order(
-            orderId = Random.nextInt(Int.MAX_VALUE),
-            userId = userId,
-            tickets = stringToIntList(selectedSeatID),
-            food = getFoodIds(),
-            scheduleId = schedule.scheduleId,
-            time = System.currentTimeMillis(),
-            discount = 0.0,
-            totalTicket = currentPrice,
-            totalFood = getTotalPrice(),
-            status = "Pending",
-            paymentMethod = "VNPay",
-            total = currentPrice + getTotalPrice(),
-        )
-        val fragment = PaymentPreviewFragment()
-        val gson = Gson()
-        val jsonOrder = gson.toJson(order) //val orderFromJson = gson.fromJson(jsonOrder, Order::class.java)
-        val args = Bundle()
-        args.putString("order", jsonOrder)
-        args.putInt("movieId",schedule.movieId)
-        args.putInt("cinemaId",schedule.cinemaId)
-        args.putString("roomId",schedule.roomId.toString())
-        Log.i("movieId",schedule.movieId.toString())
-        fragment.arguments = args;
-
-        val orderApi = RetrofitClient.instance.create(OrderApi::class.java)
-
-        val call = orderApi.uploadOrder(order)
-        call.enqueue(object : Callback<ResponseBody> {
-            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                Log.i("API", response.toString())
-                switchFragment(fragment)
+        init {
+            buttonAdd.setOnClickListener {
+                val currentAmount = textAmount.text.toString().toIntOrNull() ?: 0
+                val newAmount = currentAmount + 1
+                textAmount.text = newAmount.toString()
+                onFoodAdd.invoke(foods[adapterPosition].id)
             }
 
-            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Log.i("API", "Upload failed")
-            }
-        })
-    }
-
-
-    private fun tryPostOrder(){
-        val sharedPref = requireContext().getSharedPreferences("auth", Context.MODE_PRIVATE)
-        val token = sharedPref.getString("token", "") ?: ""
-
-        if (token != "") {
-            val accountService = RetrofitClient.instance.create(AccountApi::class.java)
-            val call = accountService.getUserDetail(token)
-            call.enqueue(object : Callback<User> {
-                override fun onResponse(call: Call<User>, response: Response<User>) {
-                    if (response.isSuccessful) {
-                        val user = response.body()!!
-                        userId = user.id
-                        SubmitData()
-                    } else {
-                        (this@FoodOrderFragment.activity as? MainActivity)?.addFragment(LoginFragment(), "login")
-                    }
-                }
-
-                override fun onFailure(call: Call<User>, t: Throwable) {
-                    Log.i("API", t.message!!)
-                }
-            })
-        }else{
-            (this@FoodOrderFragment.activity as? MainActivity)?.addFragment(LoginFragment(), "login")
-        }
-    }
-
-    private fun switchFragment(fragment: Fragment){
-        (this.activity as? MainActivity)?.addFragment(fragment, "payment-review")
-    }
-
-    private fun fetchData()
-    {
-        val foodApi = RetrofitClient.instance.create(FoodApi::class.java)
-        val call = foodApi.getAllFood()
-        call.enqueue(object : Callback<List<Food>> {
-            override fun onResponse(call: Call<List<Food>>, response: Response<List<Food>>) {
-                if (response.isSuccessful) {
-                    foodList = ArrayList<Food>(response.body()!!)
-                    Log.i("API", foodList.count().toString())
-                    init();
-                    updateOrderDetails();
-                } else {
-                    val errorMessage = response.message()
-                    Log.i("API", errorMessage)
-                    Log.i("API", "GET FAILED")
+            buttonMinus.setOnClickListener {
+                val currentAmount = textAmount.text.toString().toIntOrNull() ?: 0
+                if (currentAmount > 0) {
+                    val newAmount = currentAmount - 1
+                    textAmount.text = newAmount.toString()
+                    onFoodRemove.invoke(foods[adapterPosition].id)
                 }
             }
-
-            override fun onFailure(call: Call<List<Food>>, t: Throwable) {
-                Log.i("API", t.message!!)
-            }
-        })
-    }
-
-    private fun updateOrderDetails(){
-        Log.i("API", (currentPrice + getTotalPrice()).toString())
-        textMovieName.text = movieName
-        textPrice.text = formatToVND(currentPrice + getTotalPrice());
-        textSeatSelected.text = "${stringToIntList(selectedSeatID).count()} Seats Selected";
-    }
-
-    fun getTotalPrice(): Double {
-        var totalPrice = 0.0
-        for ((foodId, count) in foodBought) {
-            val food = foodList.find { it.id == foodId }
-            if (food != null) {
-                totalPrice += food.price * count
-            }
         }
-        return totalPrice
+
     }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewFoodAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        val view = inflater.inflate(R.layout.recyclerview_food_item_container, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return foods.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item: Food = foods[position]
+
+        holder.textFoodName.text = item.name + " - " + formatToVND(item.price);
+        holder.textFoodDescription.text = item.description;
+        Glide.with(fragment).load(item.poster).into(holder.imagePoster)
+
+        Log.i("View", "Checked");
+
+    }
+
     fun formatToVND(amount: Double): String {
         val formatter = NumberFormat.getCurrencyInstance(Locale("vi", "VN"))
         formatter.currency = Currency.getInstance("VND")
         return formatter.format(amount)
     }
-
-    fun getFoodIds(): List<Int> {
-        val foodIds = mutableListOf<Int>()
-        for ((foodId, count) in foodBought) {
-            repeat(count) {
-                foodIds.add(foodId)
-            }
-        }
-        return foodIds
-    }
-
-    fun stringToIntList(idsString: String): List<String> {
-        return idsString.split("|").map { it }
-    }
-
 }
